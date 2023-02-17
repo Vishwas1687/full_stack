32bab4d6a60172a04f81dceb1e0f72740261a855
Vishwas1687 <gpvishwas1687@gmail.com>
2023-02-17 22:42:40 +0530

    books-routes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 
 const indexRouter=require('./routes/index')
 const authorRouter=require('./routes/authors')
+const bookRouter=require('./routes/books')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
@@ -28,5 +29,6 @@ db.once('open',()=>console.log('Connected to Mongoose'))
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
 app.use('authors/new',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3000)
