e8999735bd5650fe78cb28a6927c40a59d1bfc34
Vishwas1687 <gpvishwas1687@gmail.com>
2023-02-17 18:08:06 +0530

    initial-author-routes

--- server.js
+++ server.js
@@ -8,20 +8,25 @@ const express=require('express')
 const app=express()
 const expressLayouts=require('express-ejs-layouts')
 const mongoose=require('mongoose')
+const bodyParser=require('body-parser')
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
-const indexRouter=require('./routes/index')
 
+const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 const db=mongoose.connection
 db.on('error',error=>console.log(error))
 db.once('open',()=>console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+app.use('authors/new',authorRouter)
 
 app.listen(process.env.PORT || 3000)
