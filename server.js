93dcd826e125ab7dc11fcf3e5ca500d553c87bf4
Vishwas1687 <gpvishwas1687@gmail.com>
2023-02-18 18:16:59 +0530

    finished-author-routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app=express()
 const expressLayouts=require('express-ejs-layouts')
 const mongoose=require('mongoose')
 const bodyParser=require('body-parser')
+const methodOverride=require('method-override')
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 
 const indexRouter=require('./routes/index')
@@ -21,6 +22,7 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+app.use(methodOverride('_method'))
 
 const db=mongoose.connection
 db.on('error',error=>console.log(error))
